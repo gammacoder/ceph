dc8aa53a94fab0effdc3830620dbd6295d3824e2
Roald J. van Loon <roaldvanloon@gmail.com>
2013-08-31 10:20:04 +0200

    automake cleanup: fixing build errors
    - Uninitialized field in MonitorLevelDB::Op causes random build errors.
    - Order matters; set up automake env before everything else
    - Add emergency defs of PRI*64 headers when inttypes.h does not define
      them (which, unfortunately, can happen on some systems).
    
    Signed-off-by: Roald J. van Loon <roaldvanloon@gmail.com>

--- src/include/int_types.h
+++ src/include/int_types.h
@@ -1,12 +1,54 @@
 #ifndef CEPH_INTTYPES_H
 #define CEPH_INTTYPES_H
 
+#include "acconfig.h"
+
 #if defined(__linux__)
 #include <linux/types.h>
 #endif
 
-#include <inttypes.h>
+/*
+ * Get 64b integers either from inttypes.h or glib.h
+ */
+#ifdef HAVE_INTTYPES_H
+#  include <inttypes.h>
+//#else
+//#  ifdef HAVE_GLIB
+//#    include <glib.h>
+//#  endif
+#endif
+
+/*
+ * C99 says inttypes.h includes stdint.h, but that's not true on all
+ * systems. If it's there, include it always - just in case.
+ */
+#ifdef HAVE_STDINT_H
 #include <stdint.h>
+#endif
+
+/*
+ * Emergency replacements for PRI*64 modifiers. Some systems have
+ * an inttypes.h that doesn't define all the PRI[doxu]64 macros.
+ */
+#if !defined(PRIu64)
+#  if defined(HAVE_INTTYPES) || defined(HAVE_GLIB)
+/* If we have inttypes or glib, assume we have 64-bit long long int */
+#    define PRIu64 "llu"
+#    define PRIi64 "lli"
+#    define PRIx64 "llx"
+#    define PRIX64 "llX"
+#    define PRIo64 "llo"
+#    define PRId64 "lld"
+#  else
+/* Assume that we don't have long long, so use long int modifiers */
+#    define PRIu64 "lu"
+#    define PRIi64 "li"
+#    define PRIx64 "lx"
+#    define PRIX64 "lX"
+#    define PRIo64 "lo"
+#    define PRId64 "ld"
+#  endif
+#endif
 
 #if defined(__FreeBSD__)
 #include <sys/types.h>
